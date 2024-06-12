d5e5a137a756b2d0397285bc9b0aa90e543257c4
Bahar <72806044+elmasb@users.noreply.github.com>
2024-06-12 19:28:05 +0300

    Hospital Appointment and Doctor Review Website
    This project aims to develop a website where users can book appointments from different hospital branches and review doctor profiles. It provides a user-friendly interface and ensures secure and efficient management of hospital and doctor information.

---
+++ Hospital/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebTermProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Home()
+        {
+            return View("Home", "~/Views/Shared/_Layout.cshtml");
+        }
+
+        public ActionResult About()
+        {
+            return View("About", "~/Views/Shared/_Layout.cshtml");
+        }
+        
+        public ActionResult Doctors()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View("Contact", "~/Views/Shared/_Layout.cshtml");
+        }
+    }
+}
---
+++ Hospital/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebTermProject.MvcApplication" Language="C#" %>
